c5cefdc5256d94356272c461b5b91e7e2c7212b2
Chuka <chukwukamorka@gmail.com>
2024-02-07 08:18:38 +0100

    feat: update navbar tabs

--- app/sections/navbar/tabs.tsx
+++ app/sections/navbar/tabs.tsx
@@ -51,12 +51,18 @@ export const NavbarTabs = () => {
   return (
     <div className="border-b z-50 sticky top-0 bg-brand-light border-brand-primary border-opacity-15 flex items-stretch justify-between">
       {routes.map((route) => (
-        <div className="flex-1 text-center" key={route.path}>
+        <div
+          className="flex-1 text-center justify-between gap-5"
+          key={route.path}
+        >
           <Link
-            className={cn('block text-lg font-semibold pt-2 pb-3', {
-              'border-b-2 w-brand-fit mx-auto border-brand-primary ':
-                urlHash === route.path,
-            })}
+            className={cn(
+              'inline-block w-brand-fit -mb-[1px] text-md md:text-lg font-semibold pt-2 pb-2 md:pb-3',
+              {
+                'border-b-2 mx-auto border-brand-primary ':
+                  urlHash === route.path,
+              }
+            )}
             href={createRelativeUrl(route.path)}
           >
             {route.label}
